f16539c95b94c232d827605e112163d18423bfa0
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-11 17:51:27 -0500

    Initial Commit

---
+++ SwiftUI-Accessibility/SwiftUI_AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AccessibilityApp.swift
+//  SwiftUI-Accessibility
+//
+//  Created by Rafael Calunga on 2020-12-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
